f8465a5fd060c8b43d7c0738652f6653ad879a24
Nathan Le Granvallet <nathan@legranvallet.fr>
2019-04-16 14:57:56 +0200

    + CMockOHPUTAINCAFONCTIONNE

--- CMakeModules/CodeCoverage.cmake
+++ CMakeModules/CodeCoverage.cmake
@@ -92,26 +92,10 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
     # Setup target
     ADD_CUSTOM_TARGET(${_targetname}
 
-            # Cleanup lcov
-            ${LCOV_PATH} --directory . --zerocounters
-
             # Run tests
             COMMAND ${_testrunner} ${ARGV3}
-
-            # Capturing lcov counters and generating report
-            COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
-            COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'tests/*' '/usr/*' --output-file ${_outputname}.info.cleaned
-            COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
-            COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info ${_outputname}.info.cleaned
-
+            COMMAND ${GCOV_PATH} CMakeFiles/CICDTest.dir/test/*
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            COMMENT "Resetting code coverage counters to zero.\nProcessing code coverage counters and generating report."
-            )
-
-    # Show info where to find the report
-    ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
-            COMMAND ;
-            COMMENT "Open ./${_outputname}/index.html in your browser to view the coverage report."
             )
 
 ENDFUNCTION() # SETUP_TARGET_FOR_COVERAGE
